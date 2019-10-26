95a036dca84eaeaa472ea6ac7ea44f0b5c60e7ee
Corey <mechtrondev@gmail.com>
2019-10-26 00:09:18 -0700

    Implement user create/delete/list functions for DynamoDB

--- notifier/dynamo.py
+++ notifier/dynamo.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 import os
 import sys
 import uuid
@@ -10,8 +11,8 @@ DYNAMODB_CLIENT = boto3.client('dynamodb')
 DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME')
 
 
-def get_users():
-    print("Retrieving user list..")
+def get_users_csv():
+    print("Retrieving user list from CSV..")
     users = []
     with open(os.path.join(sys.path[0], 'users.csv')) as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
@@ -20,12 +21,36 @@ def get_users():
             for col in reader.fieldnames:
                 user[col] = row[col]
             users.append(user)
+    return users
+
+
+def get_users():
+    print("Retrieving user list from DynamoDB..")
+    users = []
+    response = DYNAMODB_CLIENT.scan(
+        TableName=DYNAMODB_TABLE_NAME,
+        Select="ALL_ATTRIBUTES",
+    )
+    assert response["ResponseMetadata"]["HTTPStatusCode"] is 200
+    for user in response["Items"]:
+        users.append(dict(
+            Id=user["Id"]["S"],
+            SmsNumber=user["SmsNumber"]["S"],
+            SmsNumberIsSubscribed=user["SmsNumberIsSubscribed"]["BOOL"],
+            FavouriteTeam=user["FavouriteTeam"]["S"],
+            MinutesToNotifyBeforeGameStart=(
+                int(user["MinutesToNotifyBeforeGameStart"]["N"])
+            ),
+            LastNotified=(
+                datetime.datetime.fromtimestamp(int(user["LastNotified"]["N"]))
+            ),
+        ))
     print("{} users successfully loaded".format(len(users)))
     return users
 
 
 def create_user(**kwargs):
-    DYNAMODB_CLIENT.put_item(
+    response = DYNAMODB_CLIENT.put_item(
         TableName=DYNAMODB_TABLE_NAME,
         Item={
             "Id": {
@@ -35,16 +60,27 @@ def create_user(**kwargs):
                 "S": kwargs["sms_number"],
             },
             "SmsNumberIsSubscribed": {
-                "BOOL": kwargs["sms_is_subscribed"],
+                "BOOL": False,
             },
             "FavouriteTeam": {
-                "S": kwargs["favourite_team"],
+                "S": kwargs["team"],
             },
             "MinutesToNotifyBeforeGameStart": {
                 "N": kwargs["minutes_to_notify_before"],
             },
             "LastNotified": {
-                "N": kwargs["last_notified"],
+                "N": "0",
             },
         }
     )
+    assert response["ResponseMetadata"]["HTTPStatusCode"] is 200
+    print("New user successfully created")
+
+
+def delete_user(user_id):
+    response = DYNAMODB_CLIENT.delete_item(
+        TableName=DYNAMODB_TABLE_NAME,
+        Key={"Id": {"S": user_id}},
+    )
+    assert response["ResponseMetadata"]["HTTPStatusCode"] is 200
+    print("User with ID {} was successfully deleted".format(user_id))
